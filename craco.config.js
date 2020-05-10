d74591d4e24f5c93c6f8cec79ea5efb3700d859e
Alexander Trishin <Alexander_Trishin@mail.ru>
2020-05-10 23:07:46 +0300

    add two new custom icons

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     webpack: {
         alias: {
+            components: path.join(path.resolve(__dirname, 'src/components')),
             containers: path.join(path.resolve(__dirname, 'src/containers')),
             pages: path.join(path.resolve(__dirname, 'src/pages')),
             utils: path.join(path.resolve(__dirname, 'src/utils')),
@@ -11,6 +12,7 @@ module.exports = {
     jest: {
         configure: {
             moduleNameMapper: {
+                '^components(.*)$': '<rootDir>/src/components$1',
                 '^containers(.*)$': '<rootDir>/src/containers$1',
                 '^pages(.*)$': '<rootDir>/src/pages$1',
                 '^utils(.*)$': '<rootDir>/src/utils$1',
