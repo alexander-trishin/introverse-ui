a77e2fae26a5837bd51f4cfeca8757edf4f6e7b5
Alexander Trishin <Alexander_Trishin@mail.ru>
2020-05-30 01:20:24 +0300

    update application router

--- src/containers/Application/ApplicationRouter.tsx
+++ src/containers/Application/ApplicationRouter.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import BannerPage from 'pages/BannerPage';
 import HomePage from 'pages/HomePage';
 import NotFoundPage from 'pages/NotFoundPage';
 
 const ApplicationRouter = () => (
-    <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-    </Switch>
+    <>
+        <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route component={NotFoundPage} />
+        </Switch>
+        <Route component={BannerPage} />
+    </>
 );
 
 export default ApplicationRouter;
